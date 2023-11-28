4f92752d1666cafd4e4e74e94a872af96f31dd27
AsiaEasa <asiaeasa89@gmail.com>
2023-11-28 01:06:33 -0800

    done

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        last_num = str(number)[-1:]
+    elif number >= 0:
+        last_num = str(number)[-1:]
+    print("{:d}".format(last_num), end="")
+    return last_num
