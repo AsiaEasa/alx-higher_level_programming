85ac7c365b98caeb25b750a171fdc7848ac0d501
AsiaEasa <asiaeasa89@gmail.com>
2023-11-30 01:29:31 -0800

    done

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-from variable_load_5 import a
-
-
-def peint_find():
-    print(a)
-
-
 if __name__ == "__main__":
-    print_find()
-
+    from variable_load_5 import a
+    print(a)
