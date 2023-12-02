2a8f99069fc7ac3439d9fcc9cf007049cd75cdc8
AsiaEasa <asiaeasa89@gmail.com>
2023-12-02 02:07:22 -0800

    done

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,3 @@
-#!/usr/bin/python3
-def no_c(my_string):
-    new_str = ""
-    for i in my_string:
-        if (i != 'c') and (i != 'C'):
-            new_str += i
-    return (new_str)
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        print(' '.join('{:d}'.format(j)for j in i))
