747797df26e00662b3755eb682da8afe1b7ae559
AsiaEasa <asiaeasa89@gmail.com>
2023-12-19 02:07:00 -0800

    done

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+
+
 class Square:
+    """Defines a square."""
     def __init__(self, size=0):
+	"""Constructor.
+            size: length
+        """
         self.__size = size
         if type(size) != int:
             raise TypeError("size must be an integer")
