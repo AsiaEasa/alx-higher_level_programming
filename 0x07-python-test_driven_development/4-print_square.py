78327744b922745d7fc6428fb2e97288ee7bd62e
AsiaEasa <asiaeasa89@gmail.com>
2024-01-02 22:47:37 -0800

    done

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,21 +1,10 @@
 #!/usr/bin/python3
 """Module for print_square method."""
+
+
 def print_square(size):
     """Prints a square using the character #.
-
-    Args:
-        size (int): The size length of the square.
-
-    Raises:
-        TypeError: If size is not an integer.
-        ValueError: If size is less than 0.
-
-    Example:
-        >>> print_square(4)
-        ####
-        ####
-        ####
-        ####
+    Args: size (int): The size length of the square.
     """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
