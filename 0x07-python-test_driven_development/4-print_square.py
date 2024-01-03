9cf003690438eefa9acd40b2d6f040fa059658b1
AsiaEasa <asiaeasa89@gmail.com>
2024-01-02 23:04:51 -0800

    done

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -14,6 +14,9 @@ def print_square(size):
 
     for _ in range(size):
         print("#" * size)
+
+
 if __name__ == "__main__":
     import doctest
+
     doctest.testfile("tests/4-print_square.txt")
