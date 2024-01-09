f7398fb0a252c8aef7bce45526d3b555197d2261
AsiaEasa <asiaeasa89@gmail.com>
2024-01-09 03:03:44 -0800

    done

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -6,27 +6,9 @@ prints the following statistics:
     - Total file size up to that point.
     - Count of read status codes up to that point.
 """
+
+
 def print_stats(size, status_codes):
-    print("File size: {}".format(size))
-    for key, value in status_codes.items():
-        print("{}: {}".format(key, value))
-
-if __name__ == "__main__":
-    import sys
-
-    size = 0
-    status_codes = {}
-    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-
-    try:
-        for line in sys.stdin:
-            size += int(line.split()[-1])
-            code = line.split()[-2]
-            if code in valid_codes:
-                status_codes[code] = status_codes.get(code, 0) + 1
-
-        print_stats(size, status_codes)
-
-    except KeyboardInterrupt:
-        print_stats(size, statu
-	raise
+    print("File size:", size)
+    for key, value in sorted(status_codes.items(), key=lambda x: x[1], reverse=True):
+        print(f"{key}: {value}")
