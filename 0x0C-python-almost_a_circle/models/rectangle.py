d92efe32061e1b3748f1d4c92a071f20d62dc30a
AsiaEasa <asiaeasa89@gmail.com>
2024-01-12 02:37:09 -0800

    donw

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -17,46 +17,46 @@ class Rectangle(Base):
     @property
     def width(self):
         '''Rectangle width.'''
-        return self.__width
+        return (self.__width)
 
     @width.setter
     def width(self, value):
-        self.validate_integer("width", value, False)
+        self.val_int("width", value, False)
         self.__width = value
 
     @property
     def height(self):
         '''Rectangle height.'''
-        return self.__height
+        return (self.__height)
 
     @height.setter
     def height(self, value):
-        self.validate_integer("height", value, False)
+        self.val_int("height", value, False)
         self.__height = value
 
     @property
     def x(self):
         '''x in rectangle.'''
-        return self.__x
+        return (self.__x)
 
     @x.setter
     def x(self, value):
-        self.validate_integer("x", value)
+        self.val_int("x", value)
         self.__x = value
 
     @property
     def y(self):
         '''y in rectangle.'''
-        return self.__y
+        return (self.__y)
 
     @y.setter
     def y(self, value):
-        self.validate_integer("y", value)
+        self.val_int("y", value)
         self.__y = value
 
-    def validate_integer(self, name, value, check=True):
+    def val_int(self, name, value, check=True):
         '''Validating value in rectangle.'''
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if check and value < 0:
             raise ValueError("{} must be >= 0".format(name))
@@ -65,13 +65,13 @@ class Rectangle(Base):
 
     def area(self):
         '''Area of this rectangle.'''
-        return self.width * self.height
+        return (self.width * self.height)
 
     def display(self):
         '''Prints string representation.'''
-        s = '\n' * self.y + \
+        m = '\n' * self.y + \
             (' ' * self.x + '#' * self.width + '\n') * self.height
-        print(s, end='')
+        print(m, end='')
 
     def __str__(self):
         '''Returns string info.'''
@@ -101,5 +101,5 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         '''Returns dictionary.'''
-        return {"id": self.id, "width": self.__width, "height": self.__height,
-                "x": self.__x, "y": self.__y}
+        return ({"id": self.id, "width": self.__width, "height": self.__height,
+                "x": self.__x, "y": self.__y})
