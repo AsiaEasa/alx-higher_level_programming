5fcb37ec0940daec9574d0bcd2d34a1369d7a6e0
AsiaEasa <asiaeasa89@gmail.com>
2024-01-15 10:57:20 -0800

    done

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -10,15 +10,35 @@ import io
 
 class TestSquare(unittest.TestCase):
     '''Tests the Base class.'''
-    def test_J_display_no_args(self):
+
+    def test_is_base(self):
+        self.assertIsInstance(Square(10), Base)
+
+    def test_is_rectangle(self):
+        self.assertIsInstance(Square(10), Square)
+
+    def test_no_args(self):
+        with self.assertRaises(TypeError):
+            Square()
+
+    def test_one_arg(self):
+        s1 = Square(10)
+        s2 = Square(11)
+        self.assertEqual(s1.id, s2.id - 1)
+
+    def test_two_args(self):
+        s1 = Square(10, 2)
+        s2 = Square(2, 10)
+        self.assertEqual(s1.id, s2.id - 1)
+
+    def test_display(self):
         m = Square(10)
         with self.assertRaises(TypeError) as e:
             Square.display()
         n = "display() missing 1 required positional argument: 'self'"
         self.assertEqual(str(e.exception), n)
 
-    def test_J_display_simple(self):
-        '''Tests display() method output.'''
+    def test_display_s(self):
         m = Square(1)
         n = io.StringIO()
         with redirect_stdout(n):
