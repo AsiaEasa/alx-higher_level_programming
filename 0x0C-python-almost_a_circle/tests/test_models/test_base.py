2b8f394698002d44bd31e2b95bece439e5460dfa
AsiaEasa <asiaeasa89@gmail.com>
2024-01-15 08:18:49 -0800

    done

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -5,68 +5,43 @@ from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
 
-
 class TestBase(unittest.TestCase):
-    '''Tests the Base class.'''
+    def test_from_json_string_l(self):
+        '''Tests to_json_string() signature:'''
+        with self.assertRaises(TypeError) as m:
+            Base.from_json_string()
+        n = "from_json_string() missing 1 required positional argument: \
+'json_string'"
+        self.assertEqual(str(m.exception), n)
 
-    def setUp(self):
-        '''Imports module, instantiates class'''
-        Base._Base__nb_objects = 0
-        pass
+        self.assertEqual(Base.from_json_string(None), [])
+        self.assertEqual(Base.from_json_string(""), [])
 
-    def tearDown(self):
-        '''Cleans up after each test_method.'''
-        pass
-    def test_C_instantiation(self):
-        '''Tests Base() instantiation.'''
-        b = Base()
-        self.assertEqual(str(type(b)), "<class 'models.base.Base'>")
-        self.assertEqual(b.__dict__, {"id": 1})
-        self.assertEqual(b.id, 1)
+        n = '[{"x": 4, "y": 4, "width": 4, "id": 4, "height": 4}, \
+{"x": 10, "y": 20, "width": 34, "id": 44, "height": 340}]'
+        k = [{'x': 4, 'y': 4, 'width': 4, 'id': 4, 'height': 4},
+             {'x': 10, 'y': 20, 'width': 34, 'id': 44,
+             'height': 340}]
+        self.assertEqual(Base.from_json_string(n), k)
 
-    def test_D_constructor(self):
-        '''Tests constructor signature.'''
-        with self.assertRaises(TypeError) as e:
-            Base.__init__()
-        msg = "__init__() missing 1 required positional argument: 'self'"
-        self.assertEqual(str(e.exception), msg)
 
-    def test_D_constructor_args_2(self):
-        '''Tests constructor signature with 2 notself args.'''
-        with self.assertRaises(TypeError) as e:
-            Base.__init__(self, 1, 2)
-        msg = "__init__() takes from 1 to 2 positional arguments but 3 \
-were given"
-        self.assertEqual(str(e.exception), msg)
+    def test_from_json_string_t(self):
+        list_in = [{"id": 80, "width": 20, "height": 74}]
+        json_list_in = Rectangle.to_json_string(list_in)
+        list_out = Rectangle.from_json_string(json_list_in)
+        self.assertEqual(list, type(list_out))
 
-    def test_E_consecutive_ids(self):
-        '''Tests consecutive ids.'''
-        b1 = Base()
-        b2 = Base()
-        self.assertEqual(b1.id + 1, b2.id)
-class TestBase_to_json_string(unittest.TestCase):
-    """Unittests for testing to_json_string method of Base class."""
+    def test_from_json_string_one_rectangle(self):
+        list_in = [{"id": 80, "width": 20, "height": 74, "x": 97}]
+        json_list_in = Rectangle.to_json_string(list_in)
+        list_out = Rectangle.from_json_string(json_list_in)
+        self.assertEqual(list_in, list_out)
 
-    def test_to_json_string_rectangle_type(self):
-        r = Rectangle(10, 7, 2, 8, 6)
-        self.assertEqual(str, type(Base.to_json_string([r.to_dictionary()])))
-
-    def test_to_json_string_rectangle_one_dict(self):
-        r = Rectangle(10, 7, 2, 8, 6)
-        self.assertTrue(len(Base.to_json_string([r.to_dictionary()])) == 53)
-class TestBase_save_to_file(unittest.TestCase):
-    """Unittests for testing save_to_file method of Base class."""
-
-    def test_save_to_file_one_rectangle(self):
-        r = Rectangle(10, 7, 2, 8, 5)
-        Rectangle.save_to_file([r])
-        with open("Rectangle.json", "r") as f:
-            self.assertTrue(len(f.read()) == 53)
-
-    def test_save_to_file_two_rectangles(self):
-        r1 = Rectangle(10, 7, 2, 8, 5)
-        r2 = Rectangle(2, 4, 1, 2, 3)
-        Rectangle.save_to_file([r1, r2])
-        with open("Rectangle.json", "r") as f:
-            self.assertTrue(len(f.read()) == 105)
-
+    def test_from_json_string_two_rectangles(self):
+        list_in = [
+            {"id": 80, "width": 20, "height": 74, "x": 97, "y": 88},
+            {"id": 80, "width": 57, "height": 62, "x": 51, "y": 33},
+        ]
+        json_list_in = Rectangle.to_json_string(list_in)
+        list_out = Rectangle.from_json_string(json_list_in)
+        self.assertEqual(list_in, list_out)
