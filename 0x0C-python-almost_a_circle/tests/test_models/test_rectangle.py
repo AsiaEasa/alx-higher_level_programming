4bb323536bd9552df4ac9515ee188e22760f7583
AsiaEasa <asiaeasa89@gmail.com>
2024-01-15 11:19:43 -0800

    done

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -10,26 +10,26 @@ from models.rectangle import Rectangle
 class TestRectangle(unittest.TestCase):
     '''Tests to Base'''
 
-    def test_D_instantiation(self):
-        r = Rectangle(10, 20)
-        self.assertEqual(str(type(r)), "<class 'models.rectangle.Rectangle'>")
-        self.assertTrue(isinstance(r, Base))
-        d = {'_Rectangle__height': 20, '_Rectangle__width': 10,
+    def test_instan(self):
+        m = Rectangle(1, 30)
+        self.assertEqual(str(type(m)), "<class 'models.rectangle.Rectangle'>")
+        self.assertTrue(isinstance(m, Base))
+        n = {'_Rectangle__height': 30, '_Rectangle__width': 1,
              '_Rectangle__x': 0, '_Rectangle__y': 0, 'id': 3}
-        self.assertDictEqual(r.__dict__, d)
+        self.assertDictEqual(m.__dict__, n)
 
         with self.assertRaises(TypeError) as e:
-            r = Rectangle("1", 2)
+            m = Rectangle("11", 2)
         msg = "width must be an integer"
         self.assertEqual(str(e.exception), msg)
 
         with self.assertRaises(TypeError) as e:
-            r = Rectangle(1, "2")
+            m = Rectangle(11, "2")
         msg = "height must be an integer"
         self.assertEqual(str(e.exception), msg)
 
         with self.assertRaises(TypeError) as e:
-            r = Rectangle(1, 2, "3")
+            r = Rectangle(11, 2, "43")
         msg = "x must be an integer"
         self.assertEqual(str(e.exception), msg)
 
