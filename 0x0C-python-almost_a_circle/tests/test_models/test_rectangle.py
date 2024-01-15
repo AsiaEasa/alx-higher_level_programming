d97773014723849df9ceeb2fbaa5c84c5d083327
AsiaEasa <asiaeasa89@gmail.com>
2024-01-15 11:10:02 -0800

    done

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+'''Module Rectangle'''
+import io
+import sys
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    '''Tests to Base'''
+
+    def test_D_instantiation(self):
+        r = Rectangle(10, 20)
+        self.assertEqual(str(type(r)), "<class 'models.rectangle.Rectangle'>")
+        self.assertTrue(isinstance(r, Base))
+        d = {'_Rectangle__height': 20, '_Rectangle__width': 10,
+             '_Rectangle__x': 0, '_Rectangle__y': 0, 'id': 3}
+        self.assertDictEqual(r.__dict__, d)
+
+        with self.assertRaises(TypeError) as e:
+            r = Rectangle("1", 2)
+        msg = "width must be an integer"
+        self.assertEqual(str(e.exception), msg)
+
+        with self.assertRaises(TypeError) as e:
+            r = Rectangle(1, "2")
+        msg = "height must be an integer"
+        self.assertEqual(str(e.exception), msg)
+
+        with self.assertRaises(TypeError) as e:
+            r = Rectangle(1, 2, "3")
+        msg = "x must be an integer"
+        self.assertEqual(str(e.exception), msg)
+
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -10,27 +10,6 @@ import io
 
 class TestSquare(unittest.TestCase):
     '''Tests the Base class.'''
-
-    def test_is_base(self):
-        self.assertIsInstance(Square(10), Base)
-
-    def test_is_rectangle(self):
-        self.assertIsInstance(Square(10), Square)
-
-    def test_no_args(self):
-        with self.assertRaises(TypeError):
-            Square()
-
-    def test_one_arg(self):
-        s1 = Square(10)
-        s2 = Square(11)
-        self.assertEqual(s1.id, s2.id - 1)
-
-    def test_two_args(self):
-        s1 = Square(10, 2)
-        s2 = Square(2, 10)
-        self.assertEqual(s1.id, s2.id - 1)
-
     def test_display(self):
         m = Square(10)
         with self.assertRaises(TypeError) as e:
