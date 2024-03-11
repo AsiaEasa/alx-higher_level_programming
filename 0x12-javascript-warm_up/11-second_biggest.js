3aa77a94c08f0c0db438bb10de80abd02830365e
AsiaEasa <asiaeasa89@gmail.com>
2024-03-11 04:39:37 -0700

    done

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,6 @@ if (process.argv.length <= 3) {
   console.log('0');
 } else {
   const arr = process.argv.slice(2).map(Number);
-  const second = arr.sort(function (x, y) { return x - y; })[1];
+  const second = arr.sort(function (x, y) { return y - x; })[1];
   console.log(second);
 }
