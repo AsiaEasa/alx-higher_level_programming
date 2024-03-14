6e1beb9aeaee29ffd0af7df379799ad09af14c7b
AsiaEasa <asiaeasa89@gmail.com>
2024-03-14 13:38:16 -0700

    done

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,31 +1,27 @@
 #!/usr/bin/python3
-"""script that lists all cities from
+"""Script that lists all cities from
 the database hbtn_0e_4_usa
 """
-
+import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    import MySQLdb
-    import sys
+    USER = sys.argv[1]
+    PASS = sys.argv[2]
+    DB = sys.argv[3]
 
-    USER = argv[1]
-    PASS = argv[2]
-    DB = argv[3]
+    db = MySQLdb.connect(host="localhost", port=3306, user=USER, passwd=PASS, db=DB)
 
-    db = MySQLdb.connect(host=localhost,
-                         port=3306,
-                         user=USER,
-                         passwd=PASS,
-                         db=DB)
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT C.id, C.name, S.name \
+                    FROM cities C, states S \
+                    WHERE C.state_id = S.id \
+                    ORDER BY C.id"
+    )
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
 
-    cursor = db.cursor()
-    cursor.execute("SELECT C.id, C.name, S.name",
-                      "FROM cities C, states S",
-                      "WHERE C.state_id = S.id",
-                      "ORDER BY C.id")
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
-
-    cursor.close()
-    db.close()
+    cursor.close()
+    db.close()
