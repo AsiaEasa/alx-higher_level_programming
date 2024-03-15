0722d00002f9aa0bcd322a270f0fc2359290ff1b
AsiaEasa <asiaeasa89@gmail.com>
2024-03-15 05:40:00 -0700

    done

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -8,7 +8,7 @@ from relationship_city import City
 
 if __name__ == "__main__":
     username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-    
+
     engine = create_engine(
         f"mysql+mysqldb://{username}:{password}@localhost/{database}",
         pool_pre_ping=True,
