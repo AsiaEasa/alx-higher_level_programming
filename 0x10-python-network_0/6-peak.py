961364db8b95225fd690531edb1e0e708f2b38c8
AsiaEasa <asiaeasa89@gmail.com>
2024-03-27 05:24:08 -0700

    done

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,15 +8,14 @@ def find_peak(list_of_integers):
         return None
 
     L = 0
-    R = len(list_of_integers)
-    R -= R
+    R = len(list_of_integers) -1
 
     while L < R:
-        M = (R - L) // 2
+        M = L + (R - L) // 2
 
-        if list_of_integers[M + L] < list_of_integers[M + 1]:
-            L = M + L + 1
+        if list_of_integers[M] < list_of_integers[M + 1]:
+            L = M + 1
         else:
-            R = M + L
+            R = M
 
     return list_of_integers[L]
