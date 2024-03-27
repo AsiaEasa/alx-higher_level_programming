9df639c104203ab29cfecbbf0f2d6f5d02298d91
AsiaEasa <asiaeasa89@gmail.com>
2024-03-27 05:32:28 -0700

    done

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # script that sends a request to a URL passed as an argument
-url=$1
-
-response=$(curl -s -o /dev/null -w "%{http_code}" "$url")
-echo $response
+curl -s -o /dev/null -w "%{http_code}" $1
