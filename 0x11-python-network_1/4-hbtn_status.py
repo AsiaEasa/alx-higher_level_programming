abb1fd99e09968e136df7aefc7fc19f98a51911a
AsiaEasa <asiaeasa89@gmail.com>
2024-03-30 14:16:13 -0700

    done

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,7 +6,7 @@ import requests
 if __name__ == "__main__":
     URL = 'https://alx-intranet.hbtn.io/status'
     response = requests.get(URL)
-    
+
     print("Body response:")
     print("\t- type:", type(response.text))
     print("\t- content:", response.text)
