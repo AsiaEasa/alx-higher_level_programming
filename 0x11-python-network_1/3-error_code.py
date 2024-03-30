09415cbb0ec23f38a3df702823a0899b70343582
AsiaEasa <asiaeasa89@gmail.com>
2024-03-30 13:57:02 -0700

    done

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Python script that takes in a URL"""
-from urllib import request, error
 import sys
+import urllib.request
+import urllib.error
 
 
 if __name__ == "__main__":
