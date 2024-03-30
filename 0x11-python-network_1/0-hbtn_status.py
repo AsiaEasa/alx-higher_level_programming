c8481e3f633c68afb452fb820b7a357eef6cd63c
AsiaEasa <asiaeasa89@gmail.com>
2024-03-30 05:34:31 -0700

    done

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""script that fetches"""
+import urllib.request
+
+def main():
+"""main function"""
+    url = 'https://alx-intranet.hbtn.io/status'
+
+    req = urllib.request.Request(url)
+
+    with urllib.request.urlopen(req) as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type:", type(content))
+        print("\t- content:", content)
+        print("\t- utf8 content:", content.decode('utf-8'))
+
+if __name__ == "__main__":
+    main()
