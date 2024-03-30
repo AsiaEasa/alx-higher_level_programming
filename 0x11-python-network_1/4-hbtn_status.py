09c267e9f134164bb5c652c543bba1585ef7a27e
AsiaEasa <asiaeasa89@gmail.com>
2024-03-30 14:14:37 -0700

    done

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
+"""Python script that fetches url"""
 import requests
 
-def main():
+
+if __name__ == "__main__":
     URL = 'https://alx-intranet.hbtn.io/status'
     response = requests.get(URL)
     
     print("Body response:")
     print("\t- type:", type(response.text))
     print("\t- content:", response.text)
-
-if __name__ == "__main__":
-    main()
