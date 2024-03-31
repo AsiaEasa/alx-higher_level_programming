642a9ee910f54c4f3755b971515e2ae72564c6d5
AsiaEasa <asiaeasa89@gmail.com>
2024-03-31 09:58:09 -0700

    done

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Python script that sends a request to the URL and"""
+   import sys
+   import requests
+
+
+   if __name__ == "__main__":
+       url = "https://api.github.com/repos/{}/{}/commits".format(
+		               sys.argv[2], sys.argv[1])
+
+           M = requests.get(url)
