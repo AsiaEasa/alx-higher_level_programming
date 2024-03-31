005835e8681e764c2050670fbf5922c9b41e42f6
AsiaEasa <asiaeasa89@gmail.com>
2024-03-31 14:58:10 -0700

    done

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Sends a POST request to http://0.0.0.0:5000/search_user
+with a letter as a parameter.
+Displays the id and name from the JSON response if it's properly formatted and not empty.
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        q = ""
+    else:
+        q = sys.argv[1]
+
+    URL = 'http://0.0.0.0:5000/search_user'
+    payload = {'q': q}
+
+    res = requests.post(URL, data=payload)
+
+    try:
+        json_res = res.json()
+        if json_res:
+            print("[{}] {}".format(json_res['id'], json_res['name']))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
