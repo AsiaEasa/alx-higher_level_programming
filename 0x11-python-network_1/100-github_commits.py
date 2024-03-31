52c83eaa2ea8d16a10465ce8908677cb01292b68
AsiaEasa <asiaeasa89@gmail.com>
2024-03-31 10:01:25 -0700

    done

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""Python script that sends a request to the URL and"""
-   import sys
-   import requests
+"""Python script that sends a request to the URL and
+"""
+import sys
+import requests
 
 
-   if __name__ == "__main__":
-       url = "https://api.github.com/repos/{}/{}/commits".format(
-		               sys.argv[2], sys.argv[1])
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
-           M = requests.get(url)
+    M = requests.get(url)
