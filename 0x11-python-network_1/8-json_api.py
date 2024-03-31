96b3158de8862f054c60e85ecd815d4dd4ff7f09
AsiaEasa <asiaeasa89@gmail.com>
2024-03-31 15:01:18 -0700

    done

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """
 Sends a POST request to http://0.0.0.0:5000/search_user
-with a letter as a parameter.
-Displays the id and name from the JSON response if it's properly formatted and not empty.
 """
 import requests
 import sys
